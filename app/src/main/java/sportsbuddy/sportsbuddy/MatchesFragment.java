9b8c31de7e0f581bb819a63f2ba8b6d92b15f2ba
gesha17 <shishgeor@gmail.com>
2018-03-05 17:05:12 +0100

    Added RequestsAdapter class and MatchesAdapterClass, some core functionality of MatchesFragment.

--- app/src/main/java/sportsbuddy/sportsbuddy/MatchesFragment.java
+++ app/src/main/java/sportsbuddy/sportsbuddy/MatchesFragment.java
@@ -6,12 +6,21 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.GridView;
 import android.widget.TextView;
 
 /**
  * Created by s165700 on 2/28/2018.
  */
 
+/**
+ * Basic composition: 2 Buttons 1 GridView with 2 Adapters.
+ * When either of the buttos is pressed it changes the adapter of the gridView.
+ * For example: When the buttonViewRequests button is clicked the gridView changes its adapter from
+ * matchesAdapter to requestsAdapter. The adapter on its turn loads the information, if any, and displays it in the gridView.
+ */
 public class MatchesFragment extends Fragment {
 
     @Nullable
@@ -19,6 +28,28 @@ public class MatchesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.matches_layout, container, false);
+
+        Button buttonViewRequests = (Button) view.findViewById(R.id.buttonViewRequests);
+        Button buttonViewMatches = (Button) view.findViewById(R.id.buttonViewMatches);
+        final GridView gridView = (GridView) view.findViewById(R.id.gridView);
+
+        final MatchesAdapter matchesAdapter;
+        RequestsAdapter requestsAdapter;
+
+        buttonViewRequests.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //gridView.setAdapter(matchesAdapter);
+            }
+        });
+
+        buttonViewMatches.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //gridView.setAdapter(requestsAdapter);
+            }
+        });
+
         return view;
     }
 }
---
+++ app/src/main/java/sportsbuddy/sportsbuddy/RequestsAdapter.java
@@ -0,0 +1,35 @@
+package sportsbuddy.sportsbuddy;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by s165700 on 3/5/2018.
+ */
+
+/**
+ * Creates the items to be displayed as requests.
+ */
+
+public class RequestsAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
