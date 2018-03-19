fe8e24a899303b25eb2296d4d87e59ef3de7cc3c
gesha17 <shishgeor@gmail.com>
2018-03-19 21:46:00 +0100

    Small fix.

--- app/src/main/java/sportsbuddy/sportsbuddy/ViewProfileActivity.java
+++ app/src/main/java/sportsbuddy/sportsbuddy/ViewProfileActivity.java
@@ -12,5 +12,6 @@ public class ViewProfileActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.viewprofile_layout);
     }
 }
