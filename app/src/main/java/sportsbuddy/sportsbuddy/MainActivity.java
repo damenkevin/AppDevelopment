58287947d96362e9d17ba0fe8b255b22959f256d
damenkevin <kevindamen_1@hotmail.com>
2018-03-20 17:21:49 +0100

    BottomNavigation added

--- app/src/main/java/sportsbuddy/sportsbuddy/MainActivity.java
+++ app/src/main/java/sportsbuddy/sportsbuddy/MainActivity.java
@@ -3,20 +3,16 @@ package sportsbuddy.sportsbuddy;
 import android.app.Dialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.design.widget.TabLayout;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.firebase.ui.auth.AuthUI;
@@ -38,20 +34,9 @@ import com.google.android.gms.tasks.Task;
  */
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    private static final String SELECTED_ITEM = "menu_matches";
+    private BottomNavigationView mBottomNav;
+    private int mSelectedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,22 +45,48 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
 
         //Set up local database and init databaseHandler
         SQLiteHelper sqLiteHelper = new SQLiteHelper(getApplicationContext(), "LocalDB.sqlite",null,1);
         DatabaseHandler databaseHandler = DatabaseHandler.getDatabaseHandler();
         databaseHandler.setSqLiteHelper(sqLiteHelper);
+
+        mBottomNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        mBottomNav.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        loadFragment(new MatchesFragment());
+
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment;
+            switch (item.getItemId()) {
+                case R.id.menu_matches:
+                    fragment = new MatchesFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.menu_sscinfo:
+                    fragment = new SSCInfoFragment();
+                    loadFragment(fragment);
+                    return true;
+                case R.id.menu_timetable:
+                    fragment = new TimetableFragment();
+                    loadFragment(fragment);
+                    return true;
+            }
+
+            return false;
+        }
+    };
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SELECTED_ITEM, mSelectedItem);
+        super.onSaveInstanceState(outState);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -140,108 +151,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * A placeholder fragment containing a simple view.
+     * loading fragment into FrameLayout
+     *
+     * @param fragment
      */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            /**
-             * We are not using these, please use the Fragment classes for the individual pages. :)
-             */
-            /**
-             * The Matches Page
-             */
-            if(getArguments().getInt(ARG_SECTION_NUMBER) == 1){
-                View rootView = inflater.inflate(R.layout.matches_layout, container, false);
-
-                return rootView;
-            }
-
-            /**
-             * The ssc info page
-             */
-             else if(getArguments().getInt(ARG_SECTION_NUMBER) == 2){
-                View rootView = inflater.inflate(R.layout.ssc_info_page, container, false);
-
-                return rootView;
-            }
-
-            /**
-             * The timetable page
-             */
-            else{
-                View rootView = inflater.inflate(R.layout.timetable_page, container, false);
-
-                return rootView;
-            }
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position){
-                case 0:
-                    MatchesFragment matchesFragment = new MatchesFragment();
-                    return matchesFragment;
-                case 1:
-                    SSCInfoFragment sscInfoFragment = new SSCInfoFragment();
-                    return sscInfoFragment;
-                case 2:
-                    TimetableFragment timetableActivity = new TimetableFragment();
-                    return  timetableActivity;
-            }
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Matches";
-                case 1:
-                    return "SSC";
-                case 2:
-                    return "Timetable";
-            }
-            return null;
-        }
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
