9bdf71ada19677608704cf3f02a26eb65c4ee263
Marjac <m.jacobs@student.tue.nl>
2018-03-24 22:00:12 +0100

    Adjusted the timetable. Data handling is not perfect yet, but well on its way.

--- app/src/main/java/sportsbuddy/sportsbuddy/DatabaseHandler.java
+++ app/src/main/java/sportsbuddy/sportsbuddy/DatabaseHandler.java
@@ -12,6 +12,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Consumer;
+
 /**
  * Created by s165700 on 3/19/2018.
  */
@@ -34,15 +39,17 @@ public class DatabaseHandler {
     private static SQLiteHelper sqLiteHelper;
 
     private static UserInformation userInformation;
+    private static List<UserTimeTable> userTimeTable;
 
 
     //To make sure only one instance of the DatabaseHandler is created
-    protected static DatabaseHandler getDatabaseHandler(){
-        if(!isSetUp){
+    protected static DatabaseHandler getDatabaseHandler() {
+        if (!isSetUp) {
             database = FirebaseDatabase.getInstance();
             firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
             timeTableRef = database.getReference("TimeTableSlot");
             usersRef = database.getReference("UsersInfo");
+            userTimeTable = new ArrayList<>();
             isSetUp = true;
         }
 
@@ -64,7 +71,7 @@ public class DatabaseHandler {
     /**
      * Adds a new Timeslot to the server database
      */
-    public static void addNewTimeSlotToServerDatabase(String sport, String day, String timeFrom, String timeTo){
+    public static void addNewTimeSlotToServerDatabase(String sport, String day, String timeFrom, String timeTo) {
         //Create a timetable slot in the server database with a unique ID
         DatabaseReference newTimeTableSlot = timeTableRef.push().getRef();
         //Add the unique userID which is final and consistent with the database
@@ -75,27 +82,28 @@ public class DatabaseHandler {
         newTimeTableSlot.child("Event").child("TimeFrom").setValue(timeFrom);
         newTimeTableSlot.child("Event").child("TimeTo").setValue(timeTo);
         //Insert into the local database
-        sqLiteHelper.insertTimeTableSlotDetails(newTimeTableSlot.getKey(),sport,day,timeFrom,timeTo);
+        sqLiteHelper.insertTimeTableSlotDetails(newTimeTableSlot.getKey(), sport, day, timeFrom, timeTo);
     }
 
     /**
      * Checks for matches of a specific type
+     *
      * @param activity
      * @param day
      * @param timeFrom
      * @param timeTo
      */
-    public static void checkForMatches(final String activity,final String day, final String timeFrom, final String timeTo){
+    public static void checkForMatches(final String activity, final String day, final String timeFrom, final String timeTo) {
         timeTableRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
                     //If the current event we are looking at is not the same user
-                    if(!snapshot.child("User").getValue().equals(firebaseUser.getUid())){
-                        if(snapshot.child("Event").child("Activity").getValue().equals(activity)){
-                            if(snapshot.child("Event").child("Day").getValue().equals(day)){
+                    if (!snapshot.child("User").getValue().equals(firebaseUser.getUid())) {
+                        if (snapshot.child("Event").child("Activity").getValue().equals(activity)) {
+                            if (snapshot.child("Event").child("Day").getValue().equals(day)) {
                                 //TODO: Compare to values from local database
-                                if(Integer.valueOf(snapshot.child("Event").child("TimeFrom").getValue().toString()) < 10){
+                                if (Integer.valueOf(snapshot.child("Event").child("TimeFrom").getValue().toString()) < 10) {
 
                                 }
                             }
@@ -112,8 +120,9 @@ public class DatabaseHandler {
         });
     }
 
+
     //Upadates the server and local database with the new user info
-    public static void updateUserInfo(String name, String gender, String age, String about){
+    public static void updateUserInfo(String name, String gender, String age, String about) {
         usersRef.child(firebaseUser.getUid()).child("Name").setValue(name);
         usersRef.child(firebaseUser.getUid()).child("Gender").setValue(gender);
         usersRef.child(firebaseUser.getUid()).child("Age").setValue(age);
@@ -123,28 +132,26 @@ public class DatabaseHandler {
 
     //Gets a user from the online database. Not used ATM
     //TODO: Adapt and use this when viewing other profiles
-    public void getUserInfoFromServer(String uID){
+    public void getUserInfoFromServer(String uID) {
         usersRef.child(uID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
 
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
 
-                    if(snapshot.getKey().equals("Name")){
+                    if (snapshot.getKey().equals("Name")) {
                         userInformation.setName(snapshot.getValue().toString());
                     }
-                    if(snapshot.getKey().equals("Age")){
+                    if (snapshot.getKey().equals("Age")) {
                         userInformation.setAge(snapshot.getValue().toString());
                     }
-                    if(snapshot.getKey().equals("Gender")){
+                    if (snapshot.getKey().equals("Gender")) {
                         userInformation.setGender(snapshot.getValue().toString());
                     }
-                    if(snapshot.getKey().equals("About")){
+                    if (snapshot.getKey().equals("About")) {
                         userInformation.setAbout(snapshot.getValue().toString());
                     }
                 }
-
-
                 usersRef.removeEventListener(this);
             }
 
@@ -155,25 +162,60 @@ public class DatabaseHandler {
         });
     }
 
-    //returns an instance of the user infromation filled with data from the local database
-    public static UserInformation getUserInfoFromLocal(){
-        String name ="blank";
+    //gets user time table from database.
+    public static void getUserTimeTableFromServer(final Callback<List<UserTimeTable>> callback) {
+        timeTableRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //creates an empty list
+                userTimeTable = new ArrayList<>();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    if (Objects.equals(snapshot.child("User").getValue(), firebaseUser.getUid())) {
+
+                        String activity = snapshot.child("Event").child("Activity").getValue().toString();
+                        String day = snapshot.child("Event").child("Day").getValue().toString();
+                        String timeFrom = snapshot.child("Event").child("TimeFrom").getValue().toString();
+                        String timeTo = snapshot.child("Event").child("TimeTo").getValue().toString();
+
+                        UserTimeTable timeSlot = new UserTimeTable(activity, day, timeFrom, timeTo);
+
+                        userTimeTable.add(timeSlot);
+                    }
+                }
+                callback.call(userTimeTable);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+        });
+
+    }
+
+
+    //returns an instance of the user information filled with data from the local database
+    public static UserInformation getUserInfoFromLocal() {
+        String name = "blank";
         String age = "blank";
         String gender = "blank";
         String about = "blank";
         Cursor cursor = sqLiteHelper.getData("SELECT COUNT(*) FROM Profile");
         boolean empty = true;
         if (cursor != null && cursor.moveToFirst()) {
-            empty = (cursor.getInt (0) == 0);
+            empty = (cursor.getInt(0) == 0);
         }
         cursor.close();
-        Log.d("Reached",String.valueOf(empty));
-        if(empty){
+        Log.d("Reached", String.valueOf(empty));
+        if (empty) {
             sqLiteHelper.insertPersonalProfileInfo(FirebaseAuth.getInstance().getUid(), "Blank", "0", "Blank", "Blank");
-            Log.d("Filled The table","");
+            Log.d("Filled The table", "");
         } else {
             cursor = sqLiteHelper.getData("SELECT * FROM PROFILE");
-            while(cursor.moveToNext()) {
+            while (cursor.moveToNext()) {
                 name = cursor.getString(2);
                 age = cursor.getString(3);
                 gender = cursor.getString(4);
@@ -184,8 +226,8 @@ public class DatabaseHandler {
                 Log.d("about is: ", about);
             }
         }
-        userInformation = new UserInformation(FirebaseAuth.getInstance().getUid(),name,age,gender,about);
-        return userInformation ;
+        userInformation = new UserInformation(FirebaseAuth.getInstance().getUid(), name, age, gender, about);
+        return userInformation;
     }
 
     /*
@@ -197,7 +239,7 @@ public class DatabaseHandler {
      */
 
     //sets up the local database.
-    public static void setSqLiteHelper(SQLiteHelper _sqLiteHelper){
+    public static void setSqLiteHelper(SQLiteHelper _sqLiteHelper) {
         sqLiteHelper = _sqLiteHelper;
         sqLiteHelper.queryData
                 ("CREATE TABLE IF NOT EXISTS Slots(Id INTEGER PRIMARY KEY AUTOINCREMENT, slotID VARCHAR, activity VARCHAR, day VARCHAR, timeFrom VARCHAR, timeTo VARCHAR)");
@@ -205,6 +247,7 @@ public class DatabaseHandler {
                 ("CREATE TABLE IF NOT EXISTS Profile(Id INTEGER PRIMARY KEY AUTOINCREMENT, uID VARCHAR, name VARCHAR, age VARCHAR, gender VARCHAR, about VARCHAR)");
 
     }
+
     public static FirebaseDatabase getDatabase() {
         return database;
     }
@@ -237,6 +280,9 @@ public class DatabaseHandler {
         DatabaseHandler.userInformation = userInformation;
     }
 
+    public static List<UserTimeTable> getUserTimeTable() {
+        return userTimeTable;
+    }
     /*
      * <--- Gertters and setters END --->
      */
--- app/src/main/java/sportsbuddy/sportsbuddy/TimetableFragment.java
+++ app/src/main/java/sportsbuddy/sportsbuddy/TimetableFragment.java
@@ -27,7 +27,10 @@ import org.joda.time.format.DateTimeFormatter;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 
 /**
@@ -46,7 +49,7 @@ public class TimetableFragment extends Fragment {
         databaseHandler = DatabaseHandler.getDatabaseHandler();
         setPopUpDialog(addNewSlotButton);
         initLayout(view);
-        initListener();
+        //initListener();
         initData();
         return view;
     }
@@ -91,10 +94,7 @@ public class TimetableFragment extends Fragment {
     private TimeTableView timeTable;
 
     private ArrayList<TimeTableData> mShortSamples = new ArrayList<>();
-    private ArrayList<TimeTableData> mLongSamples = new ArrayList<>();
 
-    private List<String> mTitles = Arrays.asList("Football", "Tennis", "Hockey", "Tennis", "Football", "Tennis");
-    private List<String> mLongHeaders = Arrays.asList("Today", "Tomorrow");
     private List<String> mShortHeaders = Arrays.asList("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
 
     private long mNow = 8;
@@ -105,130 +105,26 @@ public class TimetableFragment extends Fragment {
         timeTable = (TimeTableView) view.findViewById(R.id.timeTable);
     }
 
-    private void initListener() {
-        btnMode.setOnClickListener(new View.OnClickListener() {
+    private void initData() {
+        DatabaseHandler.getUserTimeTableFromServer(new Callback<List<UserTimeTable>>() {
             @Override
-            public void onClick(View v) {
-                toogleMode();
+            public void call(List<UserTimeTable> data) {
+                loadData();
+                //initShortSamples();
 
-                if (v.isActivated()) {
-                    timeTable.setShowHeader(true);
-                    timeTable.setTableMode(TimeTableView.TableMode.SHORT);
-                    //timeTable.setTimeTable(getMillis("2017-11-10 00:00:00"), mShortSamples);
-                    timeTable.setTimeTable(mNow, getSamples(mNow, mShortHeaders, mTitles));
+                timeTable.setStartHour(8);
+                timeTable.setShowHeader(true);
+                timeTable.setTableMode(TimeTableView.TableMode.SHORT);
 
-                } else {
-                    timeTable.setShowHeader(true);
-                    timeTable.setTableMode(TimeTableView.TableMode.LONG);
-                    //timeTable.setTimeTable(getMillis("2017-11-10 00:00:00"), mLongSamples);
-                    timeTable.setTimeTable(mNow, getSamples(mNow, mLongHeaders, mTitles));
-                }
+                DateTime now = DateTime.now();
+                mNow = now.withTimeAtStartOfDay().getMillis();
+
+                timeTable.setTimeTable(mNow, mShortSamples);
             }
         });
+
     }
 
-    private void initData() {
-        //initLongSamples();
-        //initShortSamples();
-
-        timeTable.setStartHour(8);
-        timeTable.setShowHeader(true);
-        timeTable.setTableMode(TimeTableView.TableMode.LONG);
-
-        DateTime now = DateTime.now();
-        mNow = now.withTimeAtStartOfDay().getMillis();
-
-        //timeTable.setTimeTable(getMillis("2017-11-10 00:00:00"), mLongSamples);
-        timeTable.setTimeTable(mNow, getSamples(mNow, mLongHeaders, mTitles));
-    }
-
-    private ArrayList<TimeTableData> getSamples(long date, List<String> headers, List<String> titles) {
-        TypedArray colors_table = getResources().obtainTypedArray(R.array.colors_table);
-        TypedArray colors_table_light = getResources().obtainTypedArray(R.array.colors_table_light);
-
-        ArrayList<TimeTableData> tables = new ArrayList<>();
-        for (int i = 0; i < headers.size(); i++) {
-            ArrayList<TimeData> values = new ArrayList<>();
-            DateTime start = new DateTime(date);
-            DateTime end = start.plusMinutes((int) ((Math.random() * 10) + 1) * 30);
-            for (int j = 0; j < titles.size(); j++) {
-                int color = colors_table_light.getResourceId(j, 0);
-                int textColor = R.color.black;
-                //TEST
-                if (headers.size() == 2 && i == 1) {
-                    color = colors_table.getResourceId(j, 0);
-                    textColor = R.color.white;
-                }
-
-                TimeData timeData = new TimeData(j, titles.get(j), color, textColor, start.getMillis(), end.getMillis());
-
-                //TEST
-                if (headers.size() == 2 && j == 2) {
-                    timeData.setShowError(true);
-                }
-                values.add(timeData);
-
-                start = end.plusMinutes((int) ((Math.random() * 10) + 1) * 10);
-                end = start.plusMinutes((int) ((Math.random() * 10) + 1) * 30);
-            }
-
-            tables.add(new TimeTableData(headers.get(i), values));
-        }
-        return tables;
-    }
-
-    private void initLongSamples() {
-        //TEST
-        ArrayList<TimeData> values = new ArrayList<>();
-        values.add(new TimeData(0, "Korean", R.color.colorAccent, getMillis("2017-11-10 04:00:00"), getMillis("2017-11-10 05:00:00")));
-        values.add(new TimeData(1, "English", R.color.color_table_2_light, getMillis("2017-11-10 07:00:00"), getMillis("2017-11-10 08:00:00")));
-
-        ArrayList<TimeData> values2 = new ArrayList<>();
-        values2.add(new TimeData(0, "Korean", R.color.color_table_1, R.color.white, getMillis("2017-11-10 03:00:00"), getMillis("2017-11-10 06:00:00")));
-
-        TimeData timeData = new TimeData(1, "English", R.color.color_table_2, R.color.white, getMillis("2017-11-10 07:30:00"), getMillis("2017-11-10 08:55:00"));
-        timeData.setShowError(true);
-        values2.add(timeData);
-
-        values2.add(new TimeData(2, "Math", R.color.color_table_3, R.color.white, getMillis("2017-11-10 10:40:00"), getMillis("2017-11-10 11:45:00")));
-        values2.add(new TimeData(3, "Science", R.color.color_table_4, R.color.white, getMillis("2017-11-10 15:00:00"), getMillis("2017-11-10 17:10:00")));
-        values2.add(new TimeData(4, "Physics", R.color.color_table_5, R.color.white, getMillis("2017-11-10 17:30:00"), getMillis("2017-11-10 21:30:00")));
-        values2.add(new TimeData(5, "Chemistry", R.color.color_table_6, R.color.white, getMillis("2017-11-10 21:31:00"), getMillis("2017-11-10 22:45:00")));
-        values2.add(new TimeData(6, "Biology", R.color.color_table_7, R.color.white, getMillis("2017-11-10 23:00:00"), getMillis("2017-11-11 02:30:00")));
-
-        ArrayList<TimeTableData> tables = new ArrayList<>();
-        tables.add(new TimeTableData("Today", values));
-        tables.add(new TimeTableData("Tomorrow", values2));
-
-        mLongSamples.addAll(tables);
-    }
-
-    private void initShortSamples() {
-        //TEST
-        ArrayList<TimeData> values = new ArrayList<>();
-        values.add(new TimeData(0, "Korean", R.color.color_table_1_light, getMillis("2017-11-10 04:00:00"), getMillis("2017-11-10 05:00:00")));
-        values.add(new TimeData(1, "English", R.color.color_table_2_light, getMillis("2017-11-10 07:00:00"), getMillis("2017-11-10 08:00:00")));
-
-        ArrayList<TimeData> values2 = new ArrayList<>();
-        values2.add(new TimeData(0, "Korean", R.color.color_table_1_light, getMillis("2017-11-10 03:00:00"), getMillis("2017-11-10 06:00:00")));
-        values2.add(new TimeData(1, "English", R.color.color_table_2_light, getMillis("2017-11-10 07:30:00"), getMillis("2017-11-10 08:30:00")));
-        values2.add(new TimeData(2, "Math", R.color.color_table_3_light, getMillis("2017-11-10 11:40:00"), getMillis("2017-11-10 11:45:00")));
-        values2.add(new TimeData(3, "Science", R.color.color_table_4_light, getMillis("2017-11-10 18:00:00"), getMillis("2017-11-10 18:10:00")));
-        values2.add(new TimeData(4, "Physics", R.color.color_table_5_light, getMillis("2017-11-10 20:00:00"), getMillis("2017-11-10 21:30:00")));
-        values2.add(new TimeData(5, "Chemistry", R.color.color_table_6_light, getMillis("2017-11-10 21:31:00"), getMillis("2017-11-10 22:45:00")));
-        values2.add(new TimeData(6, "Biology", R.color.color_table_7_light, getMillis("2017-11-10 23:00:00"), getMillis("2017-11-11 02:30:00")));
-
-        ArrayList<TimeTableData> tables = new ArrayList<>();
-        tables.add(new TimeTableData("Sun", values));
-        tables.add(new TimeTableData("Mon", values2));
-        tables.add(new TimeTableData("Tue", values));
-        tables.add(new TimeTableData("Wed", values2));
-        tables.add(new TimeTableData("Thu", values));
-        tables.add(new TimeTableData("Fri", values2));
-        tables.add(new TimeTableData("Sat", values));
-
-        mShortSamples.addAll(tables);
-    }
 
     private void toogleMode() {
         btnMode.setActivated(!btnMode.isActivated());
@@ -244,7 +140,7 @@ public class TimetableFragment extends Fragment {
     }
 
     private DateTimeFormatter getDateTimePattern() {
-        return DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
+        return DateTimeFormat.forPattern("HH:mm");
     }
 
     // =============================================================================
@@ -257,4 +153,36 @@ public class TimetableFragment extends Fragment {
         if (v != null) v.setGravity(Gravity.CENTER);
         toast.show();
     }
+
+    private void loadData() {
+        List<UserTimeTable> data = DatabaseHandler.getUserTimeTable();
+
+        Map<String, ArrayList<TimeData>> timeDataMap = new LinkedHashMap<>();
+
+        //creates empty lists
+        timeDataMap.put("Mon", new ArrayList<TimeData>());
+        timeDataMap.put("Tue", new ArrayList<TimeData>());
+        timeDataMap.put("Wed", new ArrayList<TimeData>());
+        timeDataMap.put("Thu", new ArrayList<TimeData>());
+        timeDataMap.put("Fri", new ArrayList<TimeData>());
+        timeDataMap.put("Sat", new ArrayList<TimeData>());
+        timeDataMap.put("Sun", new ArrayList<TimeData>());
+
+        int i = 0;
+        for (UserTimeTable item : data) {
+            if (mShortHeaders.contains(item.getDay())) {
+                TimeData timeData = new TimeData(i++, item.getActivity(), R.color.color_table_1, getMillis(item.getTimeFrom()), getMillis(item.getTimeTo()));
+                timeDataMap.get(item.getDay()).add(timeData);
+            }
+
+        }
+
+        ArrayList<TimeTableData> tables = new ArrayList<>();
+        //loops trough the map and displays what is linked for each entry.
+        for (Map.Entry<String, ArrayList<TimeData>> entry : timeDataMap.entrySet()) {
+            tables.add(new TimeTableData(entry.getKey(), entry.getValue()));
+        }
+        mShortSamples.addAll(tables);
+    }
 }
+
