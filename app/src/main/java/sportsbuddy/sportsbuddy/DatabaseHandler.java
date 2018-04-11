e597e32734b09b4bda3f812c14b217354cb25e29
gesha17 <shishgeor@gmail.com>
2018-04-11 17:41:53 +0200

    Small fix.

--- app/src/main/java/sportsbuddy/sportsbuddy/DatabaseHandler.java
+++ app/src/main/java/sportsbuddy/sportsbuddy/DatabaseHandler.java
@@ -494,7 +494,7 @@ public class DatabaseHandler {
     //sets up the local database.
     public static void setSqLiteHelper(SQLiteHelper _sqLiteHelper) {
         sqLiteHelper = _sqLiteHelper;
-        //TODO: Uncomment the bottom line, delete your app and install it again if it crashes. Then comment this line back.
+        //TODO: Uncomment the line below, delete your app and install it again if it crashes. Then comment this line back.
         //sqLiteHelper.queryData("DROP TABLE Slots");
         sqLiteHelper.queryData
                 ("CREATE TABLE IF NOT EXISTS Slots(Id INTEGER PRIMARY KEY AUTOINCREMENT, slotID VARCHAR, level VARCHAR, activity VARCHAR, day VARCHAR, timeFrom VARCHAR, timeTo VARCHAR)");
